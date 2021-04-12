d0c6af43a2baa0e4654e858eb1649e2ddefc7804
Xaber <xaber@foxmail.com>
2021-04-12 16:57:45 +0800

    feat: add eslint and typescript

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'node': true,
+    'es2021': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 12,
+    'sourceType': 'module'
+  },
+  'plugins': [
+    '@typescript-eslint'
+  ],
+  'ignorePatterns': ['/dist'],
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ]
+  }
+}
