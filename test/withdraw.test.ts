aecdff8ba371f19a8c7d03630f60380c1e705386
Xaber <xaber@foxmail.com>
2021-04-21 17:33:28 +0800

    feat: add api request error test

--- test/withdraw.test.ts
+++ test/withdraw.test.ts
@@ -4,15 +4,15 @@ import { ethers } from 'ethers'
 import { ChainType } from '../src/global'
 
 const provider = new ethers.providers.InfuraProvider('kovan')
-const signer = new ethers.Wallet(ethWalletHasUSDT2.privateKey, provider)
-
-const everpay = new Everpay({
-  account: ethWalletHasUSDT2.address,
-  connectedSigner: signer,
-  debug: true
-})
 
 test(`${ethWalletHasUSDT2.address} withdraw USDT to ${ethWalletHasUSDT.address}`, async () => {
+  const signer = new ethers.Wallet(ethWalletHasUSDT2.privateKey, provider)
+  const everpay = new Everpay({
+    account: ethWalletHasUSDT2.address,
+    connectedSigner: signer,
+    debug: true
+  })
+
   return await everpay.withdraw({
     chainType: ChainType.ethereum,
     symbol: 'usdt',
@@ -22,4 +22,24 @@ test(`${ethWalletHasUSDT2.address} withdraw USDT to ${ethWalletHasUSDT.address}`
     console.log('withdrawResult', withdrawResult)
     expect(withdrawResult.status).toBe('ok')
   })
+})
+
+test(`use another ${ethWalletHasUSDT.address} singer to sign ${ethWalletHasUSDT2.address}'s tx`, async () => {
+  const signer = new ethers.Wallet(ethWalletHasUSDT.privateKey, provider)
+  const everpay = new Everpay({
+    account: ethWalletHasUSDT2.address,
+    connectedSigner: signer,
+    debug: true
+  })
+
+  await expect(
+    everpay.withdraw({
+      chainType: ChainType.ethereum,
+      symbol: 'usdt',
+      amount: 8,
+      to: ethWalletHasUSDT.address
+    })
+  )
+    .rejects
+    .toThrow('err_invalid_signature')
 })
