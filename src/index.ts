c8c41bf98cd2a3dada2a6a75fda5bb9ad152e46a
Xaber <xaber@foxmail.com>
2021-05-23 14:44:22 +0800

    fix: amount decimals

--- src/index.ts
+++ src/index.ts
@@ -90,7 +90,7 @@ class Everpay extends EverpayBase {
     const { amount, symbol } = params
     const connectedSigner = this._config?.connectedSigner as Signer
     const token = getTokenBySymbol(symbol, this._cachedInfo?.tokenList)
-    const value = utils.parseUnits(amount.toString(), token?.decimals)
+    const value = utils.parseUnits(toBN(amount).toString(), token?.decimals)
     const from = this._config.account?.toLowerCase()
     const to = this._cachedInfo?.ethLocker.toLowerCase()
     let transactionResponse: TransactionResponse
@@ -143,7 +143,7 @@ class Everpay extends EverpayBase {
       action,
       from,
       to,
-      amount: utils.parseUnits(amount.toString(), token?.decimals).toString(),
+      amount: utils.parseUnits(toBN(amount).toString(), token?.decimals).toString(),
       fee: action === EverpayAction.withdraw ? (token?.burnFee ?? '0') : '0',
       feeRecipient: this._cachedInfo?.feeRecipient.toLowerCase() ?? '',
       nonce: Date.now().toString(),
