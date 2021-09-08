9b25770d0c44d53529acf84c88bdf27789d0a530
Xaber <xaber.twt@gmail.com>
2021-09-08 14:18:56 +0800

    feat: add burnFee api interface

---
+++ test/fee.test.ts
@@ -0,0 +1,18 @@
+import Everpay from '../src/index'
+
+const everpay = new Everpay({
+  debug: true
+})
+
+test('everpey fees got correct', async () => {
+  return await everpay.fees().then(fees => {
+    expect(fees.length).toBeGreaterThan(0)
+    expect(fees.find(t => t.tokenTag.toLowerCase().includes('ethereum'))).toBeTruthy()
+  })
+})
+
+test('everpey fee got correct', async () => {
+  return await everpay.fee('ar').then(fee => {
+    expect(fee.burnFee.includes(',')).toBeTruthy()
+  })
+})
