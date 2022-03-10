a3e16c4bff8815c28b0146f445769e2ca056954e
xaber <xaber.twt@gmail.com>
2022-03-10 14:36:49 +0800

    chore: update test

--- test/withdraw.pst.test.ts
+++ test/withdraw.pst.test.ts
@@ -1,5 +1,5 @@
 import Everpay from '../src/index'
-import { arWallet2 } from './constants/wallet'
+import { arWallet2, ethWalletHasUSDT } from './constants/wallet'
 import { ChainType } from '../src/types'
 
 test(`${arWallet2.address} withdraw vrt to ${arWallet2.address}`, async () => {
@@ -37,20 +37,20 @@ test(`check ${arWallet2.address} withdraw VRT failed`, async () => {
     .toThrow('PST_WITHDARW_TO_ARWEAVE_MUST_BE_INTEGER')
 })
 
-// test(`${arWallet2.address} withdraw vrt to ethereum address ${ethWalletHasUSDT.address}`, async () => {
-//   const everpay = new Everpay({
-//     account: arWallet2.address,
-//     arJWK: arWallet2.jwk,
-//     debug: true
-//   })
+test(`${arWallet2.address} withdraw vrt to ethereum address ${ethWalletHasUSDT.address}`, async () => {
+  const everpay = new Everpay({
+    account: arWallet2.address,
+    arJWK: arWallet2.jwk,
+    debug: true
+  })
 
-//   return await everpay.withdraw({
-//     chainType: ChainType.ethereum,
-//     symbol: 'vrt',
-//     amount: '0.000010001',
-//     to: ethWalletHasUSDT.address
-//   }).then(withdrawResult => {
-//     console.log('withdrawResult', withdrawResult)
-//     expect(withdrawResult.status).toBe('ok')
-//   })
-// })
+  return await everpay.withdraw({
+    chainType: ChainType.ethereum,
+    symbol: 'vrt',
+    amount: '0.000010001',
+    to: ethWalletHasUSDT.address
+  }).then(withdrawResult => {
+    console.log('withdrawResult', withdrawResult)
+    expect(withdrawResult.status).toBe('ok')
+  })
+})
